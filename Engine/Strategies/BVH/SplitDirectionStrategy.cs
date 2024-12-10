476d5c01d8860e97e027014e4979767daa85c1b7
Jort Willemsen <jort@maurice-nienke.nl>
2024-12-10 23:08:32 +0100

    add: stack implementation for bvh

--- Engine/Strategies/BVH/SplitDirectionStrategy.cs
+++ Engine/Strategies/BVH/SplitDirectionStrategy.cs
@@ -12,16 +12,15 @@ public class SplitDirectionStrategy : IBvhStrategy
         // Create the root bounding box.
         var root = new BvhNode
         {
-            IsLeaf = false, 
-            BoundingBox = scene.GetBoundingBox(),
-            Primitives = scene.Objects.ToArray()
+            IsLeaf = false, BoundingBox = scene.GetBoundingBox(), Primitives = scene.Objects.ToArray()
         };
-        
+
         return PopulateChildren(root);
     }
 
-    private BvhNode PopulateChildren(BvhNode parent)
+    private BvhNode PopulateChildren(BvhNode root)
     {
+<<<<<<< Updated upstream
         // Base case, we return if the parent node contains 1 primitive
         if (parent.Primitives.Length <= 1)
         {
@@ -30,57 +29,68 @@ public class SplitDirectionStrategy : IBvhStrategy
         }
         
         int axisToSplit = 0;
+=======
+        Stack<BvhNode> stack = new();
+        stack.Push(root);
+>>>>>>> Stashed changes
 
-        // Determine which axis to split i.e. the longest axis.
-        for (int axis = 0; axis < 3; axis++)
+        while (stack.Count > 0)
         {
-            // If the size of the current axis is longer than the selected axis, we substitute it.
-            if (parent.BoundingBox.AxisByInt(axis).Size > parent.BoundingBox.AxisByInt(axisToSplit).Size)
-            {
-                axisToSplit = axis;
-            }
-        }
-        
-        // Find the split intervals
-        (Interval fst, Interval snd) = parent.BoundingBox.AxisByInt(axisToSplit).Split();
-        
-        var primsFst = new List<IIntersectable>();
-        var primsSnd = new List<IIntersectable>();
+            var parent = stack.Pop();
 
-        // Divide the primitives based on their position in 3D space.
-        foreach (var primitive in parent.Primitives)
-        {
-            var geometry = (Geometry.Geometry)primitive;
-            // Check if it falls inside the first or second box and append accordingly.
-            if (fst.Surrounds(geometry.Position.AxisByInt(axisToSplit)))
+            // Base case
+            if (parent.Primitives.Length <= 3)
             {
-                primsFst.Add(primitive);
+                parent.IsLeaf = true;
                 continue;
             }
-                        
-            primsSnd.Add(primitive);
+
+            int axisToSplit = 0;
+
+            // Determine which axis to split i.e. the longest axis.
+            for (int axis = 0; axis < 3; axis++)
+            {
+                // If the size of the current axis is longer than the selected axis, we substitute it.
+                if (parent.BoundingBox.AxisByInt(axis).Size > parent.BoundingBox.AxisByInt(axisToSplit).Size)
+                {
+                    axisToSplit = axis;
+                }
+            }
+
+            // Find the split intervals
+            (Interval fst, _) = parent.BoundingBox.AxisByInt(axisToSplit).Split();
+
+            var primsFst = new List<IIntersectable>();
+            var primsSnd = new List<IIntersectable>();
+
+            // Divide the primitives based on their position in 3D space.
+            foreach (IIntersectable primitive in parent.Primitives)
+            {
+                Geometry.Geometry geometry = (Geometry.Geometry)primitive;
+                // Check if it falls inside the first or second box and append accordingly.
+                if (fst.Surrounds(geometry.Position.AxisByInt(axisToSplit)))
+                {
+                    primsFst.Add(primitive);
+                    continue;
+                }
+
+                primsSnd.Add(primitive);
+            }
+
+            var boxFst = new AxisAlignedBoundingBox(primsFst.Select(i => i.GetBoundingBox()).ToArray());
+            var boxSnd = new AxisAlignedBoundingBox(primsSnd.Select(i => i.GetBoundingBox()).ToArray());
+
+            var left = new BvhNode { IsLeaf = false, Primitives = primsFst.ToArray(), BoundingBox = boxFst };
+
+            var right = new BvhNode { IsLeaf = false, Primitives = primsSnd.ToArray(), BoundingBox = boxSnd };
+
+            parent.Left = left;
+            parent.Right = right;
+
+            stack.Push(left);
+            stack.Push(right);
         }
 
-        var boxFst = new AxisAlignedBoundingBox(primsFst.Select(i => i.GetBoundingBox()).ToArray());
-        var boxSnd = new AxisAlignedBoundingBox(primsSnd.Select(i => i.GetBoundingBox()).ToArray());
-
-        var left = new BvhNode
-        {
-            IsLeaf = false,
-            Primitives = primsFst.ToArray(),
-            BoundingBox = boxFst
-        };
-        
-        var right = new BvhNode
-        {
-            IsLeaf = false,
-            Primitives = primsSnd.ToArray(),
-            BoundingBox = boxSnd
-        };
-        // Recursive step
-        parent.Left = PopulateChildren(left);
-        parent.Right = PopulateChildren(right);
-
-        return parent;
+        return root;
     }
 }

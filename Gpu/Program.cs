98737a67819ac24ab3a804b78715294ed85643b9
Silas Peters <69711739+SilasPeters@users.noreply.github.com>
2025-01-07 21:48:10 +0100

    feat: first step to shading phase

--- Gpu/Program.cs
+++ Gpu/Program.cs
@@ -28,6 +28,27 @@ Scene scene = new Scene(objects, lights);
 // Prepare input data
 const int numberOfRays = 16;
 
+nuint[] randomStates = new nuint[numberOfRays]
+{
+    // TODO generate this with random seed
+    69,
+    420,
+    10,
+    20,
+    9000,
+    90001,
+    42069,
+    804930,
+    02380923809,
+    032093,
+    480239805,
+    8934820,
+    89023402,
+    4092094389,
+    043289040,
+    03488035
+};
+
 ClFloat3[] hitPositions = new ClFloat3[numberOfRays]
 {
     new ClFloat3 { X = 1, Y = 2, Z = 3 },
@@ -51,6 +72,9 @@ ClFloat3[] hitPositions = new ClFloat3[numberOfRays]
 ClFloat3[] normals = new ClFloat3[numberOfRays]
 {
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
+    new ClFloat3 { X = 0, Y = 2, Z = 0 },
+    new ClFloat3 { X = 0, Y = 1, Z = 0 },
+    new ClFloat3 { X = 0, Y = 4, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
@@ -58,10 +82,7 @@ ClFloat3[] normals = new ClFloat3[numberOfRays]
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
-    new ClFloat3 { X = 0, Y = 1, Z = 0 },
-    new ClFloat3 { X = 0, Y = 1, Z = 0 },
-    new ClFloat3 { X = 0, Y = 1, Z = 0 },
-    new ClFloat3 { X = 0, Y = 1, Z = 0 },
+    new ClFloat3 { X = 0, Y = 6, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
     new ClFloat3 { X = 0, Y = 1, Z = 0 },
@@ -72,10 +93,10 @@ ClFloat3[] normals = new ClFloat3[numberOfRays]
 ClFloat3[] incomingRayDirections = new ClFloat3[numberOfRays]
 {
     new ClFloat3 { X = 0, Y = 0, Z = 1 },
-    new ClFloat3 { X = 0, Y = 0, Z = 1 },
-    new ClFloat3 { X = 0, Y = 0, Z = 1 },
-    new ClFloat3 { X = 0, Y = 0, Z = 1 },
-    new ClFloat3 { X = 0, Y = 0, Z = 1 },
+    new ClFloat3 { X = 0, Y = 1, Z = 1 },
+    new ClFloat3 { X = 1, Y = 0, Z = 1 },
+    new ClFloat3 { X = 0, Y = 2, Z = 1 },
+    new ClFloat3 { X = 1, Y = 2, Z = 1 },
     new ClFloat3 { X = 0, Y = 0, Z = 1 },
     new ClFloat3 { X = 0, Y = 0, Z = 1 },
     new ClFloat3 { X = 0, Y = 0, Z = 1 },
@@ -89,7 +110,27 @@ ClFloat3[] incomingRayDirections = new ClFloat3[numberOfRays]
     new ClFloat3 { X = 0, Y = 0, Z = 1 },
 };
 
-ClFloat3[] albedos = new ClFloat3[numberOfRays]
+float[] mat_albedos = new float[numberOfRays]
+{
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f,
+    6.9f
+};
+
+ClFloat3[] mat_colors = new ClFloat3[numberOfRays]
 {
     new ClFloat3 { X = 80, Y = 69, Z = 111 },
     new ClFloat3 { X = 80, Y = 69, Z = 111 },
@@ -113,16 +154,21 @@ ClFloat3[] albedos = new ClFloat3[numberOfRays]
 OpenCLManager manager = new OpenCLManager();
 manager.SetProgram("/../../../../Gpu/Programs/MaterialDiffuse.cl");
 manager.SetBuffers(
-    new OutputBuffer<nint>(manager, new nint[numberOfRays]),  // debug output
+    new OutputBuffer<nint>(manager, new nint[numberOfRays]), // debug output
+    new InputBuffer<nuint>(manager, randomStates),
     new InputBuffer<ClFloat3>(manager, hitPositions),
     new InputBuffer<ClFloat3>(manager, normals),
-    new InputBuffer<ClFloat3>(manager, incomingRayDirections),
-    new InputBuffer<ClFloat3>(manager, albedos),
-    new OutputBuffer<ClRay>(manager, new ClRay[numberOfRays]), // extensionRays
-    new OutputBuffer<ClRay>(manager, new ClRay[numberOfRays])  // shadowRays
+    new InputBuffer<float>(manager, mat_albedos),
+    new InputBuffer<ClFloat3>(manager, mat_colors),
+    new OutputBuffer<ClRay>(manager, new ClRay[numberOfRays]),// extensionRays
+    new OutputBuffer<ClRay>(manager, new ClRay[numberOfRays]), // shadowRays
+    new OutputBuffer<ClFloat3>(manager, new ClFloat3[numberOfRays]) // pixelColors
 );
-manager.SetKernel("scatter");
-manager.SetWorkSize(new nuint[2] { numberOfRays / 4, numberOfRays / 4 }, new nuint[2] { 2, 2 });
+manager.SetKernel("scatter_diffuse");
+manager.SetWorkSize(new nuint[2] {
+    (nuint) MathF.Ceiling(MathF.Sqrt(numberOfRays)),
+    (nuint) MathF.Ceiling(MathF.Sqrt(numberOfRays))
+}, new nuint[2] { 2, 2 });
 
 // Execute
 var result = manager.Execute();
